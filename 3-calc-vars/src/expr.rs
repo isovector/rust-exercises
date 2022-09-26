221eb7f2a75531bef5ac18054825b8ca29ee6b25
Sandy Maguire <sandy@sandymaguire.me>
2022-09-26 13:17:17 -0700

    implement variables

--- 3-calc-vars/src/expr.rs
+++ 3-calc-vars/src/expr.rs
@@ -1,8 +1,11 @@
+use std::collections::HashMap;
+
 use Expr::*;
 use BinOp::*;
 
 pub enum Expr {
     Lit(f64),
+    Var(String),
     Bin(BinOp, Box<Expr>, Box<Expr>),
 }
 
@@ -14,14 +17,15 @@ pub enum BinOp {
 }
 
 impl Expr {
-    pub fn eval(self) -> f64 {
+    pub fn eval(self, vars: &HashMap<String, f64>) -> f64 {
         match self {
             Lit(n) => n,
+            Var(v) => vars[&v],
             Bin(op, l, r) => match op {
-                Add => l.eval() + r.eval(),
-                Sub => l.eval() - r.eval(),
-                Mul => l.eval() * r.eval(),
-                Div => l.eval() / r.eval(),
+                Add => l.eval(vars) + r.eval(vars),
+                Sub => l.eval(vars) - r.eval(vars),
+                Mul => l.eval(vars) * r.eval(vars),
+                Div => l.eval(vars) / r.eval(vars),
             }
         }
     }
--- 3-calc-vars/src/main.rs
+++ 3-calc-vars/src/main.rs
@@ -1,9 +1,24 @@
 mod expr;
 
+use std::collections::HashMap;
+
 use crate::expr::*;
 use Expr::*;
 use BinOp::*;
 
+
 fn main() {
-    println!("{}", todo!());
+    let ast =
+        Bin(Div,
+            Box::new(
+                Bin(Mul,
+                    Box::new(
+                        Bin(Add,
+                            Box::new(Var(String::from("hello"))),
+                            Box::new(Lit(2.)))),
+                    Box::new(Lit(3.)))),
+            Box::new(Lit(4.)));
+    let vals = HashMap::from([(String::from("hello"), 1_f64)]);
+    let res = ast.eval(&vals);
+    print!("{res}")
 }
